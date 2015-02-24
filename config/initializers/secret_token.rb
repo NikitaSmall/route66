c2412445c14780353cd38f94bed543b7cdb31b69
nikitasmall <nikitasosnov@yahoo.com>
2015-02-24 14:27:39 +0200

    Init commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Route66::Application.config.secret_key_base = '6ea53e9c94aa94be1cce1da6c596a4b79aa68a854df463256d3e5ffdffb299118379ae56776c663dd93ab9a59d82e8e5ae2e6197eba24d142be1a583952ed003'
