9bf59389c0114e02bd7a0647493b1b5d34f1bf67
nikitasmall <nikitasosnov@yahoo.com>
2015-02-26 00:15:16 +0200

    UI fixed, homepage is begin to developed

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,11 @@
 class LandingController < ApplicationController
   def index
+    if params[:album]
+      album = Album.find_by_title(params[:album])
+      @photos = Photo.where(album_id: album.id)
+    else
+      @photos = Photo.first
+    end
   end
 
   def about
