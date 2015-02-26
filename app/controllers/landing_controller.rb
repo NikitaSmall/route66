21cf1e29f456d7561c7a7b0d8f00ea92132046b1
nikitasmall <nikitasosnov@yahoo.com>
2015-02-26 22:33:24 +0200

    gallery created

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -3,14 +3,32 @@ class LandingController < ApplicationController
     if params[:album]
       album = Album.find_by_title(params[:album])
       @photos = Photo.where(album_id: album.id)
+      @layout = get_layout_length(@photos.length)
     else
       @photos = Photo.first
     end
+
   end
 
   def about
   end
 
   def contacts
+  end
+
+
+  def get_layout_length(length)
+    rows = (length / 2)
+    i = 0
+    @layout = String.new("")
+    rows.times do
+      if i.odd?
+        @layout = @layout.concat('2')
+        else
+        @layout = @layout.concat('3')
+      end
+      i += 1
+    end
+    return @layout
   end
 end
