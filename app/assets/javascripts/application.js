146eaf5404a54d9e984ba21de133cc83cd409a0d
nikitasmall <nikitasosnov@yahoo.com>
2015-03-04 23:06:03 +0200

    Easter egg 1: ice king

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,23 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+var string = "";
+var first = 1;
+$(document).ready(function(){
+    $(document).keyup(function(e){
+        string+= e.which.toString();
+
+        if((string.indexOf('38384040373937396566') + 1) && first == 1) {
+            $('#easter').animate({bottom:'0px'}, {duration:500});
+            $('#easter').animate({bottom:'-10px'}, {duration:300});
+            $('#easter').animate({bottom:'0px'}, {duration:300});
+            $('#easter').animate({bottom:'-15px'}, {duration:300});
+            $('#easter').animate({bottom:'-5px'}, {duration:300});
+            $('#easter').animate({bottom:'-10px'}, {duration:300});
+            $('#easter').animate({bottom:'0px'}, {duration:300});
+            $('#easter').animate({bottom:'-250px'}, {duration:400});
+            first = 0;
+        }
+    });
+});
